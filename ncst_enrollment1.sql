97a61213eb80e21b4b4aeaa6ad0bf5cfc1bd539c
kwat0g <181074004+kwat0g@users.noreply.github.com>
2025-08-12 19:53:39 +0800

    TASAD

--- ncst_enrollment1.sql
+++ ncst_enrollment1.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 10, 2025 at 09:31 PM
+-- Generation Time: Aug 12, 2025 at 01:52 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -104,11 +104,7 @@ CREATE TABLE `enrollments` (
 --
 
 INSERT INTO `enrollments` (`id`, `student_id`, `section_id`, `school_year`, `semester`, `status`, `enrollment_type`, `date_applied`, `reference_number`) VALUES
-(15, 1, 9, '2025-2026', '1st Semester', 'rejected', 'irregular', '2025-08-04 22:37:59', NULL),
-(16, 1, 8, '2025-2026', '1st Semester', 'rejected', 'irregular', '2025-08-04 22:41:01', NULL),
-(17, 1, 8, '2025-2026', '1st Semester', 'rejected', 'irregular', '2025-08-04 22:45:06', NULL),
-(18, 1, 8, '2025-2026', '1st Semester', 'rejected', 'irregular', '2025-08-04 22:49:44', NULL),
-(19, 1, 9, '2025-2026', '1st Semester', 'pending', 'irregular', '2025-08-10 13:21:11', NULL);
+(4, 1, 8, '2025-2026', '1st Semester', 'approved', NULL, '2025-08-12 19:51:32', 'ENR-20250812-4');
 
 -- --------------------------------------------------------
 
@@ -160,15 +156,17 @@ CREATE TABLE `freshman_enrollments` (
   `consent` tinyint(1) NOT NULL DEFAULT 0,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
-  `status` enum('pending','approved','accepted','rejected') NOT NULL DEFAULT 'pending'
+  `status` enum('pending','approved','accepted','rejected','processing') NOT NULL DEFAULT 'pending',
+  `documents` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`documents`))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `freshman_enrollments`
 --
 
-INSERT INTO `freshman_enrollments` (`id`, `student_id`, `course_id`, `first_name`, `middle_name`, `last_name`, `suffix`, `birthdate`, `sex`, `civil_status`, `nationality`, `citizenship`, `place_of_birth`, `religion`, `email`, `mobile`, `region_code`, `province_code`, `city_code`, `barangay_code`, `region`, `province`, `city`, `barangay`, `address_line`, `zip`, `father_name`, `father_occupation`, `father_contact`, `mother_name`, `mother_occupation`, `mother_contact`, `guardian_name`, `guardian_relation`, `guardian_contact`, `shs_name`, `shs_track`, `preferred_sched`, `year_level`, `admission_type`, `consent`, `created_at`, `updated_at`, `status`) VALUES
-(1, '2025-00001', 1, 'Jarmaine', 'Pedwino', 'Diegas', '', '2004-01-10', 'Male', 'Single', 'Filipino', 'Filipino', 'Manila', 'Roman Catholic', 'diegas.jarmaine@gmail.com', '09123131231', '040000000', '042100000', '042103000', '042103034', 'CALABARZON', 'Cavite', 'City of Bacoor', 'Habay II', 'Habay', '4102', 'Tatay Diegas', 'Ewan', '09123131231', 'Nanay Diegas', 'Ewan', '09123131231', 'Ate Diegas', 'Sibling', '09123131231', 'Habay', 'TVL', 'Morning', '3rd Year', 'Transferee', 1, '2025-08-10 16:36:29', '2025-08-10 19:27:40', 'accepted');
+INSERT INTO `freshman_enrollments` (`id`, `student_id`, `course_id`, `first_name`, `middle_name`, `last_name`, `suffix`, `birthdate`, `sex`, `civil_status`, `nationality`, `citizenship`, `place_of_birth`, `religion`, `email`, `mobile`, `region_code`, `province_code`, `city_code`, `barangay_code`, `region`, `province`, `city`, `barangay`, `address_line`, `zip`, `father_name`, `father_occupation`, `father_contact`, `mother_name`, `mother_occupation`, `mother_contact`, `guardian_name`, `guardian_relation`, `guardian_contact`, `shs_name`, `shs_track`, `preferred_sched`, `year_level`, `admission_type`, `consent`, `created_at`, `updated_at`, `status`, `documents`) VALUES
+(1, '2025-00001', 1, 'Jarmaine', 'Pedwino', 'Diegas', '', '2004-01-10', 'Male', 'Single', 'Filipino', 'Filipino', 'Manila', 'Roman Catholic', 'diegas.jarmaine@gmail.com', '09123131231', '040000000', '042100000', '042103000', '042103034', 'CALABARZON', 'Cavite', 'City of Bacoor', 'Habay II', 'Habay', '4102', 'Tatay Diegas', 'Ewan', '09123131231', 'Nanay Diegas', 'Ewan', '09123131231', 'Ate Diegas', 'Sibling', '09123131231', 'Habay', 'TVL', 'Afternoon', '3rd Year', 'Transferee', 1, '2025-08-10 16:36:29', '2025-08-12 08:36:53', 'accepted', '{\"psa\":true,\"form138\":true,\"good_moral\":true,\"tor\":true,\"notes\":\"\"}'),
+(2, NULL, 1, 'Kurt', 'Otin', 'De Mesa', '', '2004-02-01', 'Male', 'Single', 'Chinese', 'Chinese', 'LANGIT', 'Roman Catholic', 'demesa@gmail.com', '09121232321', '040000000', '042100000', '042106000', '042106005', 'CALABARZON', 'Cavite', 'City of Dasmariñas', 'Langkaan I', 'langkantot', '4111', 'dsa', 'dsads', '09111111111', 'sdsd', 'sdsd', '09111111111', 'dsadas', 'Sibling', '09111111111', 'langkantot', 'TVL', 'Evening', '1st Year', 'Freshman', 1, '2025-08-12 07:45:30', '2025-08-12 10:03:34', 'pending', NULL);
 
 -- --------------------------------------------------------
 
@@ -199,48 +197,6 @@ CREATE TABLE `irregular_enrollments` (
   `section_id` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `irregular_enrollments`
---
-
-INSERT INTO `irregular_enrollments` (`id`, `enrollment_id`, `subject_id`, `schedule_id`, `section_id`, `created_at`) VALUES
-(79, 15, 1, 49, 8, '2025-08-04 14:40:09'),
-(80, 15, 2, 50, 8, '2025-08-04 14:40:09'),
-(81, 15, 3, 42, 9, '2025-08-04 14:40:09'),
-(82, 15, 4, 44, 9, '2025-08-04 14:40:09'),
-(83, 15, 5, 53, 8, '2025-08-04 14:40:09'),
-(84, 15, 6, 54, 8, '2025-08-04 14:40:09'),
-(85, 15, 7, 48, 9, '2025-08-04 14:40:09'),
-(86, 15, 8, 47, 9, '2025-08-04 14:40:09'),
-(87, 16, 1, 49, 8, '2025-08-04 14:41:01'),
-(88, 16, 2, 50, 8, '2025-08-04 14:41:01'),
-(89, 16, 3, 51, 8, '2025-08-04 14:41:01'),
-(90, 16, 4, 52, 8, '2025-08-04 14:41:01'),
-(91, 16, 5, 45, 9, '2025-08-04 14:41:01'),
-(92, 16, 6, 46, 9, '2025-08-04 14:41:01'),
-(93, 16, 7, 48, 9, '2025-08-04 14:41:01'),
-(94, 16, 8, 47, 9, '2025-08-04 14:41:01'),
-(111, 17, 1, 49, 8, '2025-08-04 14:46:31'),
-(112, 17, 2, 50, 8, '2025-08-04 14:46:31'),
-(113, 17, 3, 51, 8, '2025-08-04 14:46:31'),
-(114, 17, 4, 52, 8, '2025-08-04 14:46:31'),
-(115, 17, 5, 45, 9, '2025-08-04 14:46:31'),
-(116, 17, 6, 46, 9, '2025-08-04 14:46:31'),
-(117, 17, 7, 55, 8, '2025-08-04 14:46:31'),
-(118, 17, 8, 56, 8, '2025-08-04 14:46:31'),
-(127, 18, 1, 49, 8, '2025-08-10 05:04:10'),
-(128, 18, 2, 50, 8, '2025-08-10 05:04:10'),
-(129, 18, 3, 42, 9, '2025-08-10 05:04:10'),
-(130, 18, 4, 44, 9, '2025-08-10 05:04:10'),
-(131, 18, 5, 53, 8, '2025-08-10 05:04:10'),
-(132, 18, 6, 54, 8, '2025-08-10 05:04:10'),
-(133, 18, 7, 48, 9, '2025-08-10 05:04:10'),
-(134, 18, 8, 47, 9, '2025-08-10 05:04:10'),
-(135, 19, 1, 41, 9, '2025-08-10 05:21:11'),
-(136, 19, 2, 43, 9, '2025-08-10 05:21:11'),
-(137, 19, 3, 51, 8, '2025-08-10 05:21:11'),
-(138, 19, 4, 52, 8, '2025-08-10 05:21:11');
 
 -- --------------------------------------------------------
 
@@ -276,7 +232,10 @@ INSERT INTO `notifications` (`id`, `student_id`, `message`, `type`, `created_at`
 (12, 1, 'Your enrollment request has been rejected by the administrator.', 'enrollment', '2025-08-04 22:40:38', 1),
 (13, 1, 'Your enrollment request has been rejected by the administrator.', 'enrollment', '2025-08-04 22:44:58', 1),
 (14, 1, 'Your enrollment request has been rejected by the administrator.', 'enrollment', '2025-08-04 22:49:33', 1),
-(15, 1, 'Your enrollment request has been rejected by the administrator.', 'enrollment', '2025-08-10 13:17:47', 1);
+(15, 1, 'Your enrollment request has been rejected by the administrator.', 'enrollment', '2025-08-10 13:17:47', 1),
+(16, 1, 'Your enrollment request has been rejected by the administrator.', 'enrollment', '2025-08-12 18:50:06', 0),
+(17, 1, 'Your enrollment request has been rejected by the administrator.', 'enrollment', '2025-08-12 19:13:11', 0),
+(18, 1, 'Your enrollment request has been rejected by the administrator.', 'enrollment', '2025-08-12 19:13:13', 0);
 
 -- --------------------------------------------------------
 
@@ -323,22 +282,8 @@ CREATE TABLE `schedules` (
 --
 
 INSERT INTO `schedules` (`id`, `section_id`, `subject_id`, `room_id`, `day`, `start_time`, `end_time`, `type`, `instructor`) VALUES
-(41, 9, 1, 3, 'Monday', '07:00:00', '08:00:00', 'Lab', 'M1'),
-(42, 9, 3, 3, 'Monday', '08:00:00', '09:00:00', 'Lab', 'M2'),
-(43, 9, 2, 3, 'Tuesday', '07:00:00', '08:00:00', 'Lec', 'M3'),
-(44, 9, 4, 3, 'Tuesday', '08:00:00', '09:00:00', 'Lec', 'M4'),
-(45, 9, 5, 3, 'Monday', '09:00:00', '10:00:00', 'Lab', 'M5'),
-(46, 9, 6, 3, 'Tuesday', '09:00:00', '10:00:00', 'Lec', 'M6'),
-(47, 9, 8, 3, 'Monday', '10:00:00', '11:00:00', 'Lec', 'M7'),
-(48, 9, 7, 3, 'Tuesday', '10:00:00', '11:00:00', 'Lab', 'M8'),
-(49, 8, 1, 3, 'Monday', '14:00:00', '15:00:00', 'Lab', '1'),
-(50, 8, 2, 3, 'Tuesday', '14:00:00', '15:00:00', 'Lec', '2'),
-(51, 8, 3, 3, 'Monday', '15:00:00', '16:00:00', 'Lab', '3'),
-(52, 8, 4, 3, 'Tuesday', '15:00:00', '16:00:00', 'Lec', '4'),
-(53, 8, 5, 3, 'Monday', '16:00:00', '17:00:00', 'Lab', '5'),
-(54, 8, 6, 3, 'Tuesday', '16:00:00', '17:00:00', 'Lec', '6'),
-(55, 8, 7, 3, 'Monday', '17:00:00', '18:00:00', 'Lab', '7'),
-(56, 8, 8, 3, 'Monday', '18:00:00', '19:00:00', 'Lec', '8');
+(5, 8, 9, 3, 'Monday', '14:00:00', '15:00:00', 'Lec', '1'),
+(6, 8, 10, 3, 'Tuesday', '14:00:00', '15:00:00', 'Lab', '2');
 
 -- --------------------------------------------------------
 
@@ -361,8 +306,10 @@ CREATE TABLE `sections` (
 
 INSERT INTO `sections` (`id`, `name`, `year_level`, `course_id`, `schedule_type`, `status`) VALUES
 (0, 'Template', '', 1, '', 'template'),
-(8, 'BSIT-31A2', '3rd', 1, 'afternoon', 'closed'),
-(9, 'BSIT-31M1', '3rd', 1, 'morning', 'closed');
+(8, 'BSIT-31A2', '3rd', 1, 'afternoon', 'open'),
+(9, 'BSIT-31M1', '3rd', 1, 'morning', 'closed'),
+(10, 'BSIT-11A2', '1st', 1, 'afternoon', 'closed'),
+(11, 'BSIT-21E1', '2nd', 1, 'evening', 'closed');
 
 -- --------------------------------------------------------
 
@@ -390,7 +337,7 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `student_id`, `last_name`, `first_name`, `middle_name`, `suffix`, `gender`, `address`, `contact_number`, `email`, `year_level`, `course_id`) VALUES
-(1, '2025-00001', 'Bungubung', 'Ehdrian', 'Latras', '', 'Male', 'Blk 5 Lot 10', '09123456789', 'bungubung.ehdrian@ncst.edu.ph', '3rd', 1);
+(1, '2025-00001', 'Diegas', 'Jarmaine', 'Pedwino', '', 'Male', 'Habay', '09123131231', 'diegas.jarmaine@gmail.com', '3rd Year', 1);
 
 -- --------------------------------------------------------
 
@@ -414,14 +361,16 @@ CREATE TABLE `subjects` (
 --
 
 INSERT INTO `subjects` (`id`, `code`, `name`, `units`, `type`, `course_id`, `year_level`, `instructor`) VALUES
-(1, 'IT301', 'INTEG', 2, 'Lab', 1, '3rd', 'Jarmaine Diegas'),
-(2, 'IT301', 'INTEG', 1, 'Lec', 1, '3rd', 'EWAN'),
-(3, 'IT302', 'HCI', 2, 'Lab', 1, '3rd', ''),
-(4, 'IT302', 'HCI', 1, 'Lec', 1, '3rd', ''),
-(5, 'IT303', 'NETWORKING 2', 2, 'Lab', 1, '3rd', NULL),
-(6, 'IT303', 'NETWORKING 2', 1, 'Lec', 1, '3rd', NULL),
-(7, 'IT304', 'SIA', 2, 'Lab', 1, '3rd', NULL),
-(8, 'IT304', 'SIA', 1, 'Lec', 1, '3rd', NULL);
+(9, 'IT301', 'SIA', 1, 'Lec', 1, '3rd', NULL),
+(10, 'IT301', 'SIA', 2, 'Lab', 1, '3rd', NULL),
+(11, 'IT302', 'HCI', 1, 'Lec', 1, '3rd', NULL),
+(12, 'IT302', 'HCI', 2, 'Lab', 1, '3rd', NULL),
+(13, 'IT303', 'INTEG', 1, 'Lec', 1, '3rd', NULL),
+(14, 'IT303', 'INTEG', 2, 'Lab', 1, '3rd', NULL),
+(15, 'IT101', 'ComProg', 1, 'Lec', 1, '1st', NULL),
+(16, 'IT101', 'ComProg', 2, 'Lab', 1, '1st', NULL),
+(17, 'IT201', 'ComProg2', 1, 'Lec', 1, '2nd', NULL),
+(18, 'IT201', 'ComProg2', 2, 'Lab', 1, '2nd', NULL);
 
 --
 -- Indexes for dumped tables
@@ -547,19 +496,19 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `enrollments`
 --
 ALTER TABLE `enrollments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `freshman_enrollments`
 --
 ALTER TABLE `freshman_enrollments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `grades`
@@ -571,13 +520,13 @@ ALTER TABLE `grades`
 -- AUTO_INCREMENT for table `irregular_enrollments`
 --
 ALTER TABLE `irregular_enrollments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=139;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `rooms`
@@ -589,13 +538,13 @@ ALTER TABLE `rooms`
 -- AUTO_INCREMENT for table `schedules`
 --
 ALTER TABLE `schedules`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `sections`
 --
 ALTER TABLE `sections`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `students`
@@ -607,7 +556,7 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `subjects`
 --
 ALTER TABLE `subjects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Constraints for dumped tables
